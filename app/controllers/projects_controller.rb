1bf9574a0d16284fa1207e0a495d5d1ca41c7bb4
aetherus <sasabune0122@gmail.com>
2012-08-31 22:33:34 +0800

    Added the ability to show all projects.

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,26 @@
+class ProjectsController < ApplicationController
+  def index
+    @projects = Project.all
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(params[:project])
+    if @project.save
+    #flash[:notice] = "Project has been created."
+    #redirect_to @project
+    #The code above can be shortened as:
+      redirect_to @project, notice: "Project has been created."
+    else
+      flash[:alert] = "Project has not been created."
+      render action: "new"
+    end
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+end
